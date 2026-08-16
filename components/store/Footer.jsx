e0a661367c11b9cd86b3ce6ff98fe5dbfe5a5828
Sayain-Aditya <as4316664@gmail.com>
2026-08-16 13:16:21 +0530

    Address

--- components/store/Footer.jsx
+++ components/store/Footer.jsx
@@ -128,7 +128,7 @@ export default function Footer() {
               www.roboflytech.in
             </a>
             <p className="text-xs text-gray-600 leading-relaxed">
-              First Floor, Harra Ki Chungi,<br />Balrampur, Azamgarh, UP, India
+              First Floor (Above SBI Building),<br />PSC Mod, Pahadpur, Balrampur,<br />Azamgarh, Uttar Pradesh - 276001
             </p>
             <p className="text-xs text-gray-600">Mon – Sat: 10:00 AM – 7:00 PM</p>
           </div>
